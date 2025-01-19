4aaf7746b9390d9749697aa4d76d7bad2743e8c7
BlueWe11s <lshepetr@gmail.com>
2025-01-19 21:49:13 +0300

    a

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['89.169.169.184', '127.0.0.1', 'localhost', 'gaylord.myvnc.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
